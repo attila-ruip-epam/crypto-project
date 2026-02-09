f1fff79f0a8b3939a32cc44d256a6b17a9bf0a4e
Attila Ruip <attila_ruip@epam.com>
2026-02-09 15:36:13 +0100

    XMCY update javadoc

--- crypto-service/controller/src/main/java/com/xmcy/controller/validator/CryptoValidator.java
+++ crypto-service/controller/src/main/java/com/xmcy/controller/validator/CryptoValidator.java
@@ -5,6 +5,9 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.util.Set;
 
+/**
+ * Validates crypto input based on configured, supported cryptos
+ */
 public class CryptoValidator {
 
     private final Set<String> supportedCryptos;
@@ -13,6 +16,9 @@ public class CryptoValidator {
         this.supportedCryptos = supportedCryptos;
     }
 
+    /**
+     * @param symbol identifying crypto to validate
+     */
     public void validate(String symbol) {
         if (StringUtils.isBlank(symbol) || !supportedCryptos.contains(symbol)) {
             throw new ValidationException();
